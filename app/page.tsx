3e0de8376da56e9ab578761b68d544a952a3ca50
JUANMANTECA <ivammartin06@gmail.com>
2026-09-16 19:58:58 +0200

    Primer commit: subida limpia

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Page() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-[#5a2a7a] p-6">
+      <div className="flex h-[520px] w-[520px] max-w-[80vw] max-h-[80vw] items-center justify-center rounded-[0px] bg-[#d62828] shadow-2xl">
+        <p
+          className="px-10 text-center text-xl leading-relaxed text-white"
+          style={{ fontFamily: '"Comic Sans MS", "Comic Sans", cursive' }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </main>
+  )
+}
